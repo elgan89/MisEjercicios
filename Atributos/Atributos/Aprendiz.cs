9459882a6d4c46428e6886bfe86c07d530b62a94
Eliecer Chavarro Torres <echavarro50@misnea.edu.co>
2020-03-02 12:35:19 -0500

    GuardarAtributos

---
+++ Atributos/Atributos/Aprendiz.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+    class Aprendiz
+    {
+    public string TipoIdentificacion {get; set;}
+    public string NumeroIdentificacion {get; set;}
+    public DateTime FechaNacimiento {get; set;}
+    public string Nombres {get; set;}
+    public string Apellidos {get; set;}
+    public string Genero {get; set;}
+    public string IdeFecha {get; set;}
+    public string Estado {get; set;}
+    public string EstadoCivil {get; set;}
+    public string Direccion {get; set;}
+    public string RH {get; set;}
+    public string Email {get; set;}
+    public string Telefono {get; set;}
+    public string TipoPoblacion {get; set;}
+    public string DepartamentoResidencia {get; set;}
+    public string MunicipioResidencia {get; set;}
+    public string DepartamentoNacimiento {get; set;}
+    public string MunicipioNacimiento {get; set;}
+    public string Estrato {get; set;}  
+    public string EPS {get; set;}
+    public string FechaIngreso {get; set;}
+    public string Discapacidad {get; set;}
+    public string LibretaMilitar {get; set;}
+    }
+
---
+++ Atributos/Atributos/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Aprendiz apSena = new Aprendiz();
+            apSena.Nombres = "Eliecer";
+            apSena.Apellidos = "Chavarro Torres";
+            apSena.Direccion = "Carrera 5 N. 8-45";
+            Saludo(apSena);
+        }
+        private static void Saludo(Aprendiz apSena)
+        {
+            Console.WriteLine("Bienvendo, sr(a) " + apSena.Nombres);
+            Console.WriteLine("Su Direccion es " + apSena.Direccion);
+            Console.ReadLine();
+        }
+    }
+
